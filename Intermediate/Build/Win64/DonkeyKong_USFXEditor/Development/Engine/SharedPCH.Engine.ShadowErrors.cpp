684b4b99198f56061de9358c5de7af077a8c85af
JoelPorcel <porcelj589@gmail.com>
2024-11-11 19:20:44 -0400

    adapter

---
+++ Intermediate/Build/Win64/DonkeyKong_USFXEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.41.34123
+#include "C:/Users/joelp/OneDrive/Documentos/PATRONES/ADAPTER/Intermediate/Build/Win64/DonkeyKong_USFXEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
